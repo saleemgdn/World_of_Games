ece5c2097b2ada5cf9d368db1d3959a013648a5b
saleemgdn <120337117+saleemgdn@users.noreply.github.com>
2023-02-04 00:54:22 +0200

    Update e2e.py

--- test/e2e.py
+++ test/e2e.py
@@ -1,5 +1,4 @@
 from selenium.webdriver.common.by import By
-
 from Utils import SCORES_FILE_NAME, BAD_RETURN_CODE
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
